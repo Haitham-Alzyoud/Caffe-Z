77fd2d54df3df5ed39b14fdaec72bc4e581ed26f
HaithamAlzyoud <haitham.alzyoud@gmail.com>
2023-09-30 01:34:51 +0300

    created a website that takes inputs by prompt with js

---
+++ app.js
@@ -0,0 +1,26 @@
+// Ask for user's name
+const name = prompt("Please enter your name:");
+
+// Ask for user's gender
+const gender = prompt("Please enter your gender (male or female):").toLowerCase();
+
+// Display a welcoming message based on gender
+if (gender === "male") {
+    alert(`Welcome, Mr. ${name}!`);
+} else if (gender === "female") {
+    alert(`Welcome, Ms. ${name}!`);
+} else {
+    alert(`Welcome, ${name}! (Title not specified)`);
+}
+
+// Ask for hot or cold drink preference
+const drinkType = prompt("Would you like a hot or cold drink?").toLowerCase();
+
+// Ask for the name of the drink
+const drinkName = prompt("Please enter the name of the drink you want:");
+
+// Alert user that their drink is getting prepared
+alert(`Your ${drinkType} ${drinkName} is getting prepared!`);
+
+// Print a statement to the console
+console.log(`${name} ordered a ${drinkType} ${drinkName}.`);
